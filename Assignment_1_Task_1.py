300ef04affac9aa800f9939798be432aac4a01f6
Om7698 <monparaom05@gmail.com>
2025-12-29 15:45:00 +0530

    Message

---
+++ Assignment_1_Task_1.py
@@ -0,0 +1,17 @@
+#Basic Mathematical Opreations
+
+num1 = int(input("Enter the first Number: "))
+num2 = int(input("Enter the second Number: "))
+
+print("Addition: ", num1+num2)
+
+"""or
+addition= num1+ num2
+print("Addition: ", addition)
+
+We can do all the operation both ways.
+
+"""
+print("Subtraction: ", num1-num2)
+print("Multiplication: ", num1*num2)
+print("Division: ", num1/num2)
---
+++ Assignment_1_Task_2.py
@@ -0,0 +1,9 @@
+# Greetings with combaine Strings
+
+first_name= input("Enter Your First Name: ")
+last_name= input("Enter Your Last Name: ")
+
+
+# print("Hello,", first_name, last_name, "How are you? I hope you are doing great Mr.", first_name, last_name)
+
+print(f"Hello, {first_name} {last_name}!, How are you? I hope You are doing Great Mr.{first_name} {last_name}.")
